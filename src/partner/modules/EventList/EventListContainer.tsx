cf306e64af9e6c3988eb2a1a746adce6d3a39c27
Victor Burgos <victor.burgos@ksquareinc.com>
2019-01-15 12:36:15 -0600

    Fix: Used asyn/await for promises

--- src/partner/modules/EventList/EventListContainer.tsx
+++ src/partner/modules/EventList/EventListContainer.tsx
@@ -6,31 +6,40 @@ import EventList from "./EventList";
 interface IEventListContainerState {
   events: IEvent[];
   isLoading: boolean;
+  error?: Error;
 }
 
 class EventListContainer extends React.Component<{}, IEventListContainerState> {
   state = {
     events: [],
-    isLoading: false
+    isLoading: false,
+    error: undefined
   };
 
-  public componentDidMount() {
+  CompDateEvent = (a: IEvent, b: IEvent) => {
+    return a.startDate > b.startDate ? -1 : a.startDate < b.startDate ? 1 : 0;
+  };
+
+  public async componentDidMount() {
     this.setState({ isLoading: true });
-    eventService.getAll().then(events => {
-      events.sort(function(a, b) {
-        return a.startDate > b.startDate
-          ? -1
-          : a.startDate < b.startDate
-          ? 1
-          : 0;
-      });
+    try {
+      const events = await eventService.getAll();
+      events.sort(this.CompDateEvent);
       this.setState({ events, isLoading: false });
-    });
+    } catch (err) {
+      this.setState({
+        isLoading: false,
+        error: err
+      });
+    }
   }
 
   public render() {
     if (this.state.isLoading) {
       return <p>is loading</p>;
+    }
+    if (this.state.error) {
+      return <p>There is a error</p>;
     }
     return <EventList events={this.state.events} />;
   }
--- src/partner/views/CurrentEventsView.tsx
+++ src/partner/views/CurrentEventsView.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "../modules/Header";
-import EventList from "../modules/EventList";
+import EventListContainer from "../modules/EventList";
 import NavBar from "../modules/NavBar";
 
 const CurrentEventsView = () => {
   return (
     <React.Fragment>
       <Header title="Current Events" />
-      <EventList />
+      <EventListContainer />
       <NavBar />
     </React.Fragment>
   );
