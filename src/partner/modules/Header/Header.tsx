ff62fcad3b667ee868ab21280c804a60aa9ec0df
Victor Burgos <victor.burgos@ksquareinc.com>
2019-01-15 12:16:26 -0600

    Fix: Avoided inline styles in header

--- src/partner/modules/Header/Header.tsx
+++ src/partner/modules/Header/Header.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Redirect } from "react-router";
+import {
+  HeaderContainer,
+  HeaderLogoContainer,
+  ImgLogo,
+  HeaderTitleContainer,
+  H1,
+  HeaderUserIconContainer,
+  ImgUserPhoto
+} from "../ui/Header/HeaderIU";
 
 interface IHeaderProps {
   title: string;
@@ -8,88 +16,23 @@ interface IHeaderProps {
 const Header: React.SFC<IHeaderProps> = props => {
   return (
     <header>
-      <div
-        className="header-container"
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-around",
-          alignItems: "center",
-          padding: "5px 3px 25px 3px",
-          background: "linear-gradient(to right, #F8823D, #E83E5D)",
-          width: "100vw",
-          height: "6em"
-        }}
-      >
-        <div
-          className="header-logo-container"
-          style={{
-            boxSizing: "border-box",
-            height: "100%",
-            width: "33.33%",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center"
-          }}
-        >
-          <img
+      <HeaderContainer>
+        <HeaderLogoContainer>
+          <ImgLogo
             src={require("../../../images/poc-chef-logo.png")}
             alt="Poc-Chef"
-            style={{
-              width: "100%",
-              height: "100%",
-              objectFit: "contain"
-            }}
           />
-        </div>
-
-        <div
-          className="header-title-container"
-          style={{
-            boxSizing: "border-box",
-            padding: "5px",
-            height: "100%",
-            width: "33.33%",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-        >
-          <h1
-            style={{
-              fontFamily: "Roboto",
-              color: "white",
-              fontSize: "16px"
-            }}
-          >
-            Current Events
-          </h1>
-        </div>
-        <div
-          className="header-user-icon-container"
-          style={{
-            boxSizing: "border-box",
-            padding: "5px",
-            height: "100%",
-            width: "33.33%",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "flex-end"
-          }}
-        >
-          <img
+        </HeaderLogoContainer>
+        <HeaderTitleContainer>
+          <H1>{props.title}</H1>
+        </HeaderTitleContainer>
+        <HeaderUserIconContainer>
+          <ImgUserPhoto
             src={require("../../../images/user-logo.png")}
             alt="Poc-Chef"
-            style={{
-              width: "2.5em",
-              height: "2.5em",
-              objectFit: "contain"
-            }}
           />
-        </div>
-      </div>
+        </HeaderUserIconContainer>
+      </HeaderContainer>
     </header>
   );
 };
