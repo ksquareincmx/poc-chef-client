2ad8a5117fe3d64698e0693aa16745272c5a8d7e
Jorge Arturo César Martínez <cdialike@gmail.com>
2019-01-17 11:29:56 -0600

    create event

--- src/components/event/Create.tsx
+++ src/components/event/Create.tsx
@@ -18,6 +18,8 @@ export class CreateEvent extends React.Component {
     event: {}
   };
 
+
+
   public eventName = React.createRef<HTMLInputElement>();
   public startDate = React.createRef<HTMLInputElement>();
   public endDate = React.createRef<HTMLInputElement>();
@@ -57,48 +59,163 @@ export class CreateEvent extends React.Component {
 
   public render() {
     const Input = styled.input({
-      position: "relative",
-      "& :hover": {
-        outline: "none",
-        boxShadow: "0.2rem 0.8rem 1.6rem",
-        background: "black",
-        color: "red"
-      }
+      borderRadius: "8px 8px 8px 8px",
+      border: "1px solid #eee",
+      marginBottom: "15px",
+      width: "88%",
+      height: "40px",
+      float: "left",
+      padding: "0px 15px"
     });
+
+    const Form = styled.form({
+      borderRadius: "5px",
+      maxWidth:"700px",
+      width:"100%",
+      margin: "5% auto",
+      backgroundColor: "#FFFFFF",
+      overflow: "hidden"
+    })
+
+    const DivMin = styled.div({
+      overflow: "0px 15px hidden",
+      clear: "both",
+      float: "right"
+    })
+    const DivMax = styled.div({
+      overflow: "hidden",
+      clear: "both"
+    })
+
+    const Button = styled.button({
+      backgroundColor: "#81BDA4",
+      color: "#FFF",
+      textAlign: "center",
+      width: "100%",
+      padding: "17px 25px",
+      borderRadius: "8px 8px 8px 8px",
+      cursor: "pointer",
+      marginTop: "40px",
+      fontSize: "18px"
+    })
+
+    const DivRCMin = styled.div({
+      width:"23%",
+      float:"right",
+      boxSizing: "border-box",
+      padding: "0px 20px 0px -15px"
+    })
+
+    const H4 = styled.h4({
+      textAlign: "left",
+      color: "#666",
+      textShadow: "1px 1px 0px #FFF",
+      fontFamily: "Arial",
+      margin:"40px 0px 0px 0px",
+      borderBottom: "1px solid black"
+    })
+
+    const H1 = styled.h1({
+      textAlign:"left",
+      color: "#666",
+      textShadow: "1px 1px 0px #FFF",
+      margin:"50px 0px 0px 0px",
+      fontFamily: "Arial",
+      borderBottom: "1px solid black"
+    })
+
+    const DivLeftC = styled.div({
+      width:"49.5%",
+      float:"left",
+      boxSizing: "border-box",
+      padding: "0px 15px 0px 0px"
+    })
+
+    const DivRightC = styled.div({
+      width:"49.5%",
+      float:"right",
+      boxSizing: "border-box",
+      padding: "0px 0px 0px 15px"
+    })
+
+    const DivCF = styled.div({
+      padding: "40px 30px",
+      fontFamily: "Arial"
+    })
+
+    const DivFG = styled.div({
+      overflow: "hidden",
+      clear: "both"
+    })
 
     return (
       <div>
-        <form onSubmit={this.createEvent}>
-          <div>
-            <label htmlFor="inputName">Event Name:</label>
-            <Input ref={this.eventName} type="text" id="inputName" />
-          </div>
-          <div>
-            <label htmlFor="inputName">Start date:</label>
-            <Input ref={this.startDate} type="text" id="inputStartDate" />
-          </div>
-          <div>
-            <label htmlFor="inputName">End date:</label>
-            <Input ref={this.endDate} type="text" id="inputEndDate" />
-          </div>
-          <div>
-            <label htmlFor="inputName">Start hour:</label>
-            <Input ref={this.startHour} type="text" id="inputStartHour" />
-          </div>
-          <div>
-            <label htmlFor="inputName">End hour:</label>
-            <Input ref={this.endHour} type="text" id="inputEndHour" />
-          </div>
-          <div>
-            <label htmlFor="inputName">Torta de Poc-chuc:</label>
-            <Input ref={this.tortaPocchuc} type="text" id="inputTortaPocchuc" />
-          </div>
-          <div>
-            <label htmlFor="inputName">Torta de Camarón:</label>
-            <Input ref={this.tortaCamaron} type="text" id="inputTortaCamaron" />
-          </div>
-          <button type="submit" />
-        </form>
+        <Form onSubmit={this.createEvent}>
+          <H1>New Event</H1>
+          <DivCF>
+            <DivMax>
+              <p>Event Name:</p>
+              <Input type="text" ref={this.eventName} />
+            </DivMax>
+
+            <DivLeftC>
+
+              <DivFG>
+                <p>Start Event:</p>
+                <Input type="date" ref={this.startDate}  />
+              </DivFG>
+              <DivFG>
+                <p>Start Hour:</p>
+                <Input type="date" ref={this.startHour} />
+              </DivFG>
+              <br />
+
+            </DivLeftC>
+
+            <DivRightC>
+
+              <DivFG>
+                <p>End Date:</p>
+                <Input type="date" ref={this.endDate} />
+              </DivFG>
+
+              <DivFG>
+                <p>End Hour:</p>
+                <Input type="date" ref={this.endHour} />
+              </DivFG>
+
+            </DivRightC>
+          </DivCF>
+
+          <H4>Price</H4>
+
+          <DivCF>
+            <DivLeftC>
+              <DivFG>
+                <br />
+                <p>Tortas Poc chuc</p>
+              </DivFG>
+              <br />
+              <DivFG>
+                <p>Tortas Camarón </p>
+              </DivFG>
+            </DivLeftC>
+
+            <DivRCMin>
+              <DivMin>
+                <Input type="text" ref={this.tortaPocchuc} />
+              </DivMin>
+
+              <DivMin>
+                <Input type="text" ref={this.tortaCamaron} />
+              </DivMin>
+            </DivRCMin>
+          </DivCF>
+
+          <Button type="submit">Create Event</Button>
+
+        </Form>
+
       </div>
     );
   }
