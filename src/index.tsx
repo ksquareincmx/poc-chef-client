1de1ba210f1883e526347ae6d8c3774e4845e2f0
Jorge Arturo César Martínez <cdialike@gmail.com>
2019-01-18 13:16:23 -0600

    fixed conflicts

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,22 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./components/App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import UserApp from "./components/App";
+import PartnerApp from "./partner/App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
-registerServiceWorker();
+ReactDOM.render(
+  <Router>
+    <React.Fragment>
+      <Route path="/user/" exact component={UserApp} />
+      <Route path="/partner/" exact component={PartnerApp} />
+    </React.Fragment>
+  </Router>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
