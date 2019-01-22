2349fa56240e4d97e76727eaef366abfb5120fc8
Victor Burgos <victor.burgos@ksquareinc.com>
2019-01-22 11:14:54 -0600

    FIx: Padding bottom of the cards

--- src/partner/modules/ui/Commons/Commons.tsx
+++ src/partner/modules/ui/Commons/Commons.tsx
@@ -1,15 +1,16 @@
-import styled from "@emotion/styled";
+import styledComponents from "styled-components";
 
-export const CardContainer = styled.div({
+export const CardContainer = styledComponents.div({
   boxShadow: "-2px 2px 10px #bdbdbd",
   background: "white",
   width: "100%",
   borderRadius: "10px",
   marginBottom: "10px",
+  paddingBottom: "10px",
   boxSizing: "border-box"
 });
 
-export const RowDataContainer = styled.div({
+export const RowDataContainer = styledComponents.div({
   width: "100%",
   padding: "5px 20px"
 });
