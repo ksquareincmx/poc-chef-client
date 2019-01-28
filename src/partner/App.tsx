c3d66a25857631d53a83d04005670de1df3b35ae
Victor Burgos <victor.burgos@ksquareinc.com>
2019-01-28 10:58:48 -0600

    Fix: Moved style for body from App.tsx to index.css

--- src/partner/App.tsx
+++ src/partner/App.tsx
@@ -3,7 +3,6 @@ import { Route, Switch } from "react-router-dom";
 import CurrentEventsView from "./views/CurrentEventsView";
 import Header from "./modules/Header";
 import NavBar from "./modules/NavBar";
-import { SpawnSyncOptionsWithBufferEncoding } from "child_process";
 
 interface IPartnerAppProps {
   match: {
@@ -15,7 +14,6 @@ const PartnerApp: React.SFC<IPartnerAppProps> = props => {
   return (
     <div>
       <Header title="New Event" />
-      <style>{"body { background-color: WhiteSmoke; }"}</style>
       <Switch>
         <Route
           path={`${props.match.url}/current-events`}
