57825155caa51af97f21ea7064640dc77005b2f2
Roberto Cervera <roberto.cervera@ksquareinc.com>
2019-03-05 09:33:08 -0600

    comments solved

--- src/components/event/Create.tsx
+++ src/components/event/Create.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import styled from "@emotion/styled";
-import styledCComponents from "styled-components";
 import { IEvent, event } from "src/partner/models/Event";
 import * as utils from "./utils";
 import cuid from "cuid";
