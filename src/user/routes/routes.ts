6e7ac5e7e02b7f2a347d1effbc729dc6371087a8
Andrés <andres.lopez@ksquareinc.com>
2019-03-26 16:29:29 -0600

    fix: correct user order view route from /order to /order/:id

--- src/user/routes/routes.ts
+++ src/user/routes/routes.ts
@@ -2,4 +2,4 @@ import { userRoute } from "src/common/routes";
 
 export const profileUserRoute = `${userRoute}/profile`;
 export const myOrdersUserRoute = `${userRoute}/my-orders`;
-export const orderViewUserRoute = `${userRoute}/order`;
+export const orderViewUserRoute = `${userRoute}/order/:id`;
