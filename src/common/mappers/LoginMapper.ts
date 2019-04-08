a50f8e6e9edf8098c59c125b5fd8a65792666dc3
Andrés <andres.lopez@ksquareinc.com>
2019-04-08 16:32:29 -0500

    add handle errors

--- src/common/mappers/LoginMapper.ts
+++ src/common/mappers/LoginMapper.ts
@@ -1,11 +1,23 @@
-import { ILoginResponseDTO, ILoginResponse } from "../models/Login";
+import { ILoginResponseDTO, ILoginResponse, ILoginErrorField } from "../models/Login";
 
 export const toEntity = (dto: ILoginResponseDTO): ILoginResponse => {
-  if (dto.errors) {
-    const errors = Array.isArray(dto.errors) ? dto.errors : [dto.errors];
-    return { errors };
+  if (dto.status !== 200) {
+    return { errors: getErrors(dto) };
   } else {
     const { user, jwt } = dto.data;
     return { user, jwt };
   }
+};
+
+const getErrors = (dto: ILoginResponseDTO): ILoginErrorField[] => {
+  let errors: ILoginErrorField[] = [];
+  if (dto.status === 401 && dto.message) {
+    errors.push({ field: "password", error: "Invalid password" });
+  }
+  if (dto.errors) {
+    const fielErrors = Array.isArray(dto.errors) ? dto.errors : [dto.errors];
+    errors.push(...fielErrors);
+  }
+
+  return errors;
 };
