e0629d17cbfbfca4129400895c8d85cd9bc2cabb
Andrés <andres.lopez@ksquareinc.com>
2019-04-08 15:15:17 -0500

    remove: userProvider

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,14 @@ import UserApp from "./user/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import { rootRoute, userRoute, partnerRoute } from "./common/routes";
-import { UserProvider } from "./common/providers";
 
 ReactDOM.render(
   <Router>
-    <UserProvider>
-      <Switch>
-        <Route path={partnerRoute} component={PartnerApp} />
-        <Route path={userRoute} component={UserApp} />
-        <Route route={rootRoute} component={() => <Redirect to="/partner/current-events" />} />
-      </Switch>
-    </UserProvider>
+    <Switch>
+      <Route path={partnerRoute} component={PartnerApp} />
+      <Route path={userRoute} component={UserApp} />
+      <Route route={rootRoute} component={() => <Redirect to="/partner/current-events" />} />
+    </Switch>
   </Router>,
   document.getElementById("root")
 );
